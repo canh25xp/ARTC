08ad787592046b653f0cde8fb3299376bf23dfff
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-12-01 16:56:34 +0000

    demo: add macro examples

--- demo/nvim/marcro/robot.cpp
+++ demo/nvim/marcro/robot.cpp
@@ -3,6 +3,14 @@ class ROBOT {
   int xpos;
   int ypos;
 
+  /*
+  Step 1: remove 'else'
+  vif                     select inside function
+  :'<,'>s/else /          search and delete all 'else'
+  Step 2:
+  [f                      goto begin of the function
+  +                       goto first word nextline
+  */
   void move(ROBOT *rob) {
     if (rob->dir == 'N') {
       rob->ypos += 1;
@@ -15,6 +23,23 @@ class ROBOT {
     }
     else if (rob->dir == 'W') {
       rob->xpos -= 1;
+    }
+  }
+
+  void move2(ROBOT *rob) {
+    switch (rob->dir) {
+      case 'N':
+        rob->ypos += 1;
+        break;
+      case 'S':
+        rob->ypos -= 1;
+        break;
+      case 'E':
+        rob->xpos += 1;
+        break;
+      case 'W':
+        rob->xpos -= 1;
+        break;
     }
   }
 };
